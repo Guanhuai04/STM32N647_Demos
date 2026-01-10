d9e64592be681d125bb54ef7cbaa10b401d44eda
Guanhuai04 <guanhuai1006@126.com>
2026-01-10 21:46:13 +0800

    refactor: remove FSBL from 03_Serial

--- Projects/03_Serial/Appli/Core/Src/main.c
+++ Projects/03_Serial/Appli/Core/Src/main.c
@@ -18,13 +18,15 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-#include "stm32n6xx_hal_dma.h"
+#include "gpdma.h"
+#include "usart.h"
+#include "xspim.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <stdint.h>
-#include <string.h>
+#include "bsp_led.h"
+#include "bsp_uart.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -41,24 +43,16 @@
 /* USER CODE BEGIN PM */
 
 /* USER CODE END PM */
+extern DMA_HandleTypeDef handle_GPDMA1_Channel0 ;
+extern DMA_HandleTypeDef handle_GPDMA1_Channel1 ;
 
 /* Private variables ---------------------------------------------------------*/
-
-UART_HandleTypeDef huart1;
-DMA_HandleTypeDef handle_GPDMA1_Channel1;
-DMA_HandleTypeDef handle_GPDMA1_Channel0;
-
-XSPI_HandleTypeDef hxspi1;
 
 /* USER CODE BEGIN PV */
 __attribute__((aligned(32))) uint8_t received_data[32];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
-static void MX_GPIO_Init(void);
-static void MX_GPDMA1_Init(void);
-static void MX_XSPI1_Init(void);
-static void MX_USART1_UART_Init(void);
 static void SystemIsolation_Config(void);
 /* USER CODE BEGIN PFP */
 
@@ -70,10 +64,12 @@ static void SystemIsolation_Config(void);
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
-int main(void) {
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -100,7 +96,6 @@ int main(void) {
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_GPDMA1_Init();
-  MX_XSPI1_Init();
   MX_USART1_UART_Init();
   SystemIsolation_Config();
   /* USER CODE BEGIN 2 */
@@ -123,285 +118,60 @@ int main(void) {
 }
 
 /**
- * @brief GPDMA1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPDMA1_Init(void) {
-  /* USER CODE BEGIN GPDMA1_Init 0 */
+  * @brief RIF Initialization Function
+  * @param None
+  * @retval None
+  */
+  static void SystemIsolation_Config(void)
+{
 
-  /* USER CODE END GPDMA1_Init 0 */
+/* USER CODE BEGIN RIF_Init 0 */
 
-  /* Peripheral clock enable */
-  __HAL_RCC_GPDMA1_CLK_ENABLE();
-
-  /* GPDMA1 interrupt Init */
-  HAL_NVIC_SetPriority(GPDMA1_Channel0_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(GPDMA1_Channel0_IRQn);
-  HAL_NVIC_SetPriority(GPDMA1_Channel1_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(GPDMA1_Channel1_IRQn);
-
-  /* USER CODE BEGIN GPDMA1_Init 1 */
-
-  /* USER CODE END GPDMA1_Init 1 */
-  /* USER CODE BEGIN GPDMA1_Init 2 */
-
-  /* USER CODE END GPDMA1_Init 2 */
-}
-
-/**
- * @brief RIF Initialization Function
- * @param None
- * @retval None
- */
-static void SystemIsolation_Config(void) {
-  /* USER CODE BEGIN RIF_Init 0 */
-
-  /* USER CODE END RIF_Init 0 */
+/* USER CODE END RIF_Init 0 */
 
   /* set all required IPs as secure privileged */
   __HAL_RCC_RIFSC_CLK_ENABLE();
 
-  /*RIMC configuration*/
-  RIMC_MasterConfig_t RIMC_master = {0};
-  RIMC_master.MasterCID = RIF_CID_0;
-  RIMC_master.SecPriv = RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_NPRIV;
-  HAL_RIF_RIMC_ConfigMasterAttributes(RIF_MASTER_INDEX_DMA2D, &RIMC_master);
-
   /* RIF-Aware IPs Config */
 
   /* set up PWR configuration */
-  HAL_PWR_ConfigAttributes(PWR_ITEM_0, PWR_SEC_NPRIV);
+  HAL_PWR_ConfigAttributes(PWR_ITEM_0,PWR_SEC_NPRIV);
 
   /* set up GPDMA configuration */
   /* set GPDMA1 channel 0 used by USART1 */
-  if (HAL_DMA_ConfigChannelAttributes(&handle_GPDMA1_Channel0,
-                                      DMA_CHANNEL_SEC | DMA_CHANNEL_PRIV |
-                                          DMA_CHANNEL_SRC_SEC |
-                                          DMA_CHANNEL_DEST_SEC) != HAL_OK) {
+  if (HAL_DMA_ConfigChannelAttributes(&handle_GPDMA1_Channel0,DMA_CHANNEL_SEC|DMA_CHANNEL_PRIV|DMA_CHANNEL_SRC_SEC|DMA_CHANNEL_DEST_SEC)!= HAL_OK )
+  {
     Error_Handler();
   }
   /* set GPDMA1 channel 1 used by USART1 */
-  if (HAL_DMA_ConfigChannelAttributes(&handle_GPDMA1_Channel1,
-                                      DMA_CHANNEL_SEC | DMA_CHANNEL_PRIV |
-                                          DMA_CHANNEL_SRC_SEC |
-                                          DMA_CHANNEL_DEST_SEC) != HAL_OK) {
+  if (HAL_DMA_ConfigChannelAttributes(&handle_GPDMA1_Channel1,DMA_CHANNEL_SEC|DMA_CHANNEL_PRIV|DMA_CHANNEL_SRC_SEC|DMA_CHANNEL_DEST_SEC)!= HAL_OK )
+  {
     Error_Handler();
   }
 
   /* set up GPIO configuration */
-  HAL_GPIO_ConfigPinAttributes(GPIOE, GPIO_PIN_5,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOE, GPIO_PIN_6,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOE, GPIO_PIN_10,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOG, GPIO_PIN_10,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_0,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_1,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_2,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_3,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_4,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_5,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_6,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_8,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_9,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_10,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPION, GPIO_PIN_11,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOO, GPIO_PIN_0,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOO, GPIO_PIN_2,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOO, GPIO_PIN_4,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOO, GPIO_PIN_5,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOP, GPIO_PIN_0,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOP, GPIO_PIN_1,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOP, GPIO_PIN_2,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOP, GPIO_PIN_3,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOP, GPIO_PIN_4,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOP, GPIO_PIN_5,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOP, GPIO_PIN_6,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
-  HAL_GPIO_ConfigPinAttributes(GPIOP, GPIO_PIN_7,
-                               GPIO_PIN_SEC | GPIO_PIN_NPRIV);
+  HAL_GPIO_ConfigPinAttributes(GPIOE,GPIO_PIN_5,GPIO_PIN_SEC|GPIO_PIN_NPRIV);
+  HAL_GPIO_ConfigPinAttributes(GPIOE,GPIO_PIN_6,GPIO_PIN_SEC|GPIO_PIN_NPRIV);
+  HAL_GPIO_ConfigPinAttributes(GPIOE,GPIO_PIN_10,GPIO_PIN_SEC|GPIO_PIN_NPRIV);
+  HAL_GPIO_ConfigPinAttributes(GPIOG,GPIO_PIN_10,GPIO_PIN_SEC|GPIO_PIN_NPRIV);
 
-  /* USER CODE BEGIN RIF_Init 1 */
+/* USER CODE BEGIN RIF_Init 1 */
 
-  /* USER CODE END RIF_Init 1 */
-  /* USER CODE BEGIN RIF_Init 2 */
+/* USER CODE END RIF_Init 1 */
+/* USER CODE BEGIN RIF_Init 2 */
 
-  /* USER CODE END RIF_Init 2 */
-}
+/* USER CODE END RIF_Init 2 */
 
-/**
- * @brief USART1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_USART1_UART_Init(void) {
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
-  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&huart1) != HAL_OK) {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) !=
-      HAL_OK) {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) !=
-      HAL_OK) {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK) {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART1_Init 2 */
-
-  /* USER CODE END USART1_Init 2 */
-}
-
-/**
- * @brief XSPI1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_XSPI1_Init(void) {
-  /* USER CODE BEGIN XSPI1_Init 0 */
-
-  /* USER CODE END XSPI1_Init 0 */
-
-  XSPIM_CfgTypeDef sXspiManagerCfg = {0};
-  XSPI_HyperbusCfgTypeDef sHyperBusCfg = {0};
-
-  /* USER CODE BEGIN XSPI1_Init 1 */
-
-  /* USER CODE END XSPI1_Init 1 */
-  /* XSPI1 parameter configuration*/
-  hxspi1.Instance = XSPI1;
-  hxspi1.Init.FifoThresholdByte = 4;
-  hxspi1.Init.MemoryMode = HAL_XSPI_SINGLE_MEM;
-  hxspi1.Init.MemoryType = HAL_XSPI_MEMTYPE_HYPERBUS;
-  hxspi1.Init.MemorySize = HAL_XSPI_SIZE_256MB;
-  hxspi1.Init.ChipSelectHighTimeCycle = 2;
-  hxspi1.Init.FreeRunningClock = HAL_XSPI_FREERUNCLK_DISABLE;
-  hxspi1.Init.ClockMode = HAL_XSPI_CLOCK_MODE_0;
-  hxspi1.Init.WrapSize = HAL_XSPI_WRAP_32_BYTES;
-  hxspi1.Init.ClockPrescaler = 1 - 1;
-  hxspi1.Init.SampleShifting = HAL_XSPI_SAMPLE_SHIFT_NONE;
-  hxspi1.Init.DelayHoldQuarterCycle = HAL_XSPI_DHQC_DISABLE;
-  hxspi1.Init.ChipSelectBoundary = HAL_XSPI_BONDARYOF_NONE;
-  hxspi1.Init.MaxTran = 0;
-  hxspi1.Init.Refresh = 0;
-  hxspi1.Init.MemorySelect = HAL_XSPI_CSSEL_NCS1;
-  if (HAL_XSPI_Init(&hxspi1) != HAL_OK) {
-    Error_Handler();
-  }
-  sXspiManagerCfg.nCSOverride = HAL_XSPI_CSSEL_OVR_NCS1;
-  sXspiManagerCfg.IOPort = HAL_XSPIM_IOPORT_1;
-  sXspiManagerCfg.Req2AckTime = 1;
-  if (HAL_XSPIM_Config(&hxspi1, &sXspiManagerCfg,
-                       HAL_XSPI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) {
-    Error_Handler();
-  }
-  sHyperBusCfg.RWRecoveryTimeCycle = 7;
-  sHyperBusCfg.AccessTimeCycle = 7;
-  sHyperBusCfg.WriteZeroLatency = HAL_XSPI_LATENCY_ON_WRITE;
-  sHyperBusCfg.LatencyMode = HAL_XSPI_VARIABLE_LATENCY;
-  if (HAL_XSPI_HyperbusCfg(&hxspi1, &sHyperBusCfg,
-                           HAL_XSPI_TIMEOUT_DEFAULT_VALUE) != HAL_OK) {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN XSPI1_Init 2 */
-
-  /* USER CODE END XSPI1_Init 2 */
-}
-
-/**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void) {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOE_CLK_ENABLE();
-  __HAL_RCC_GPIOP_CLK_ENABLE();
-  __HAL_RCC_GPIOO_CLK_ENABLE();
-  __HAL_RCC_GPIOG_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
-
-  /*Configure GPIO pin : LED_GREEN_Pin */
-  GPIO_InitStruct.Pin = LED_GREEN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_GREEN_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LED_RED_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_RED_GPIO_Port, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart) {
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
   if (huart == &huart1) {
     HAL_UART_Transmit_IT(huart, received_data, 2);
 
     GPIO_PinState state =
-        (received_data[1] == '1') ? GPIO_PIN_RESET : GPIO_PIN_SET;
+      (received_data[1] == '1') ? GPIO_PIN_RESET : GPIO_PIN_SET;
 
     if (received_data[0] == 'R') {
       HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, state);
@@ -412,7 +182,7 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart) {
   }
 }
 
-void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef* huart, uint16_t Size) {
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
   if (huart == &huart1) {
     // important: invalidate the D-Cache for the received buffer to ensure data
     SCB_InvalidateDCache_by_Addr(received_data, sizeof(received_data));
@@ -427,10 +197,11 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef* huart, uint16_t Size) {
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
-void Error_Handler(void) {
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
@@ -440,13 +211,14 @@ void Error_Handler(void) {
 }
 #ifdef USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
-void assert_failed(uint8_t* file, uint32_t line) {
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line
      number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
